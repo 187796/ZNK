854a570a38f64df27a81f03662b7f44b9b997e75
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-10-09 16:12:09 +0200

    dodanie szkieletu menu logowania

--- app/src/main/java/com/znk/znk/MainActivity.java
+++ app/src/main/java/com/znk/znk/MainActivity.java
@@ -1,7 +1,9 @@
 package com.znk.znk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mainScreen(View v) {
+        if (v.getId() == R.id.loginButton) {
+            Intent intentSimple = new Intent(getApplicationContext(), MainScreen.class);
+            startActivity(intentSimple);
+        }
     }
 }
